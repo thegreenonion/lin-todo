8fc1ac4a5d003bc7c2ccaae2c72e0d7f0d7edcff
thegreenonion <3mil.ulb3rt@gmx.de>
2024-10-12 15:27:55 +0200

    Create main.php for the project structure

---
+++ main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo Liste</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        h1 {
+            font-weight: bold;
+            text-align: center;
+            margin-top: 10px;
+        }
+        p {
+            margin-left: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="">
+        Willkommen bei der TODO-Applikation von
+        <a style="color: red" href="https://github.com/thegreenonion">thegreenonion</a>
+        ,
+        <a style="color: purple" href="https://github.com/skeund89">skeund89</a>
+        und
+        <a style="color: orange" href="https://github.com/leg0batman">leg0batman</a>
+    </h1>
+    <br>
+    <p>Ein Login ist erforderlich.</p>
+</body>
+</html>
