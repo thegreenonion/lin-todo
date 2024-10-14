d12ca942e6e3b18b2288822e5faab5b9a145e054
thegreenonion <3mil.ulb3rt@gmx.de>
2024-10-14 16:57:09 +0200

    Replace database connections with include()

--- login.php
+++ login.php
@@ -1,21 +1,7 @@
 <?php
 function Connect()
 {
-    $datenbank = "eulbert_gtodo";
-    $host = "localhost";
-    $user = "hwalde";
-    $passwd = "UG2aepai4g";
-    
-    // Datenbankverbindung herstellen
-    try 
-    {
-        $db = new PDO("mysql:dbname=$datenbank;host=$host", $user, $passwd);
-        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) 
-    {
-        die("Datenbankverbindung gescheitert: " . $e->getMessage());
-    }
-
+    include("conn.php");
     return $db;
 }
 
--- signup.php
+++ signup.php
@@ -1,18 +1,6 @@
 <?php
 include("Hash.php");
-
-$datenbank = "eulbert_gtodo";
-$host = "localhost";
-$user = "hwalde";
-$passwd = "UG2aepai4g";
-
-// Datenbankverbindung herstellen
-try {
-    $db = new PDO("mysql:dbname=$datenbank;host=$host", $user, $passwd);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Datenbankverbindung gescheitert: " . $e->getMessage());
-}
+include("conn.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
