eb6a80506a762d43da5e1733c62cb1e0bb68bae6
skeund89 <rosi.kaufmann.12@gmail.com>
2024-10-22 19:39:46 +0200

    Refactor database connection in signup.php

--- signup.php
+++ signup.php
@@ -1,18 +1,8 @@
 <?php
 include 'Hash.php';
 
-$datenbank = "eulbert_gtodo";
-$host = "localhost";
-$user = "hwalde";
-$passwd = "UG2aepai4g";
-
-// Datenbankverbindung herstellen
-try {
-  $db = new PDO("mysql:dbname=$datenbank;host=$host", $user, $passwd);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  die("Datenbankverbindung gescheitert: " . $e->getMessage());
-}
+// Create connection to db
+include("conn.php");
 
 function signup($db, $username, $password)
 {
