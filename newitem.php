8dfd22bd979dcc5d5a3d4472a5a263c52b5cf092
thegreenonion <3mil.ulb3rt@gmx.de>
2024-10-28 23:28:31 +0100

    items dürfen nur erstellt werden, wenn liste vorhanden

--- newitem.php
+++ newitem.php
@@ -4,6 +4,7 @@ include("conn.php");
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css">
+
 <body>
     <h1>Neues Item zu Liste hinzufügen:</h1>
     <form action="control/setitem.php" method="post">
@@ -14,7 +15,7 @@ include("conn.php");
         <input type="date" id="due" name="due" required>
         <br>
         <label for="lid">Liste:</label>
-        <select name="lid" id="lid">
+        <select name="lid" id="lid" required>
             <?php
             $sql = "SELECT * FROM lists WHERE lBID = ?";
             $stmt = $db->prepare($sql);
